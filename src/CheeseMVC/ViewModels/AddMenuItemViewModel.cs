4e49ff2fe3581dc22664e17fe34a1e7ef0b0aea8
Christian Lowry <shadowkappa@gmail.com>
2017-04-20 22:15:48 -0500

    Studio all done

---
+++ src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddMenuItemViewModel
+    {
+        public int cheeseID { get; set; }
+        public int menuID { get; set; }
+
+        public Menu Menu { get; set; }
+        public List<SelectListItem> Cheeses { get; set; }
+
+        public AddMenuItemViewModel() { }
+
+        public AddMenuItemViewModel(Menu aMenu, IEnumerable<Cheese> theSelectListCheeses)
+        {
+            Menu = aMenu;
+
+            Cheeses = new List<SelectListItem>();
+
+            foreach (var cheese in theSelectListCheeses)
+            {
+                Cheeses.Add(new SelectListItem
+                {
+                    Value = cheese.ID.ToString(),
+                    Text = cheese.Name
+                });
+            }
+
+        }
+
+    }
+}
